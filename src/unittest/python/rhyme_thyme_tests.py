6f49a878d13e920d98ed00d9d3a4f585346f1f44
larsvens <larsvens@kth.se>
2017-03-28 14:40:06 +0200

    improved test coverage for rhyme_thyme

--- src/unittest/python/rhyme_thyme_tests.py
+++ src/unittest/python/rhyme_thyme_tests.py
@@ -1,11 +1,13 @@
 import unittest
-
+from nltk.corpus import words
 from rhyme_thyme import RhymeThyme
 
 class test_rhyme_thyme(unittest.TestCase):
 
     def test_rhyme_thyme(self):
-        self.assertEqual(RhymeThyme.rhyme_thyme(self,'abcd'), 'Thyme\n', 'Not returning rhyming word')
+        self.assertTrue(type(RhymeThyme.rhyme_thyme(self,'abcd') == str), 'Not returning a string')
+        self.assertTrue(len(RhymeThyme.rhyme_thyme(self, 'abcd')) > 0, 'Returning empty word')
+        self.assertTrue((RhymeThyme.rhyme_thyme(self,'pear') in words.words()) or RhymeThyme.rhyme_thyme(self,'pear') == 'Failed to find rhyming word...', 'Not returning proper english word')
 
     def test_goodbye(self):
         text = 'Goodbye!\n'
